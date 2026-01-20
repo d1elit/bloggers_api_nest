93d000af9014545afee7710e7a6481ee3b20bc49
D1elit <dropdox12@gmail.com>
2026-01-20 15:52:37 +0100

    feat: add basic login usecase

---
+++ src/modules/user-accounts/application/usecases/login.usecase.ts
@@ -0,0 +1,114 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { InjectModel } from '@nestjs/mongoose';
+import {
+  User,
+  UserDocument,
+  type UserModelType,
+} from '../../domain/user.entity';
+import { authInput } from '../../api/input-dto/auth/auth.input-dto';
+import { DomainException } from '../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
+import crypto from 'node:crypto';
+import { jwtDecode } from 'jwt-decode';
+import { LoginInput } from '../../api/input-dto/auth/login.input.dto';
+import type { SessionModelType } from '../../domain/session.entity';
+import { UsersRepository } from '../../infrastructure/users.repository';
+import { CryptoService } from '../crypto.service';
+import { JwtService } from '../jwt.service';
+import { SessionsRepository } from '../../infrastructure/sessions.repository';
+
+export class LoginUserCommand {
+  constructor(public inputDto: authInput) {}
+}
+
+@CommandHandler(LoginUserCommand)
+export class LoginUserUseCase implements ICommandHandler<
+  LoginUserCommand,
+  string[]
+> {
+  constructor(
+    @InjectModel(User.name)
+    private UserModel: UserModelType,
+    private SessionModel: SessionModelType,
+    private sessionsRepository: SessionsRepository,
+    private usersRepository: UsersRepository,
+    private cryptoService: CryptoService,
+    private jwtService: JwtService,
+  ) {}
+
+  async execute(command: LoginUserCommand) {
+    const {inputDto} = command
+
+    const user = await this.checkUserCredentials(inputDto.loginDto);
+    if (!user) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        extensions: [
+          {
+            field: 'login',
+            message: 'Wrong login or password',
+          },
+        ],
+      });
+    }
+
+    const deviceId = crypto.randomUUID();
+
+    const accessToken = await this.jwtService.createAccessToken(
+      user._id.toString(),
+    );
+    const refreshToken = await this.jwtService.createRefreshToken(
+      user._id.toString(),
+      deviceId,
+    );
+    const { exp, iat } = jwtDecode(refreshToken);
+
+    const session = this.SessionModel.createNew({
+      deviceId,
+      inputDto.deviceName,
+      userId: user._id.toString(),
+      inputDto.ip,
+      iat: iat!,
+      exp: exp!,
+    });
+    await this.sessionsRepository.create(session);
+
+    return [accessToken, refreshToken];
+  }
+  async checkUserCredentials(loginDto: LoginInput): Promise<UserDocument> {
+    const user = await this.verifyLoginOrEmail(loginDto.loginOrEmail);
+    const isPasswordVerified = await this.cryptoService.comparePassword({
+      password: loginDto.password,
+      hash: user.passwordHash,
+    });
+
+    if (!user || !isPasswordVerified) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        extensions: [
+          {
+            field: 'login',
+            message: 'Wrong login or password',
+          },
+        ],
+      });
+    }
+    return user;
+  }
+
+  async verifyLoginOrEmail(login: string): Promise<UserDocument> {
+    const user = await this.usersRepository.findByLoginOrEmail(login);
+    if (!user) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        extensions: [
+          {
+            field: 'login',
+            message: 'Wrong login or password',
+          },
+        ],
+      });
+    }
+    return user;
+  }
+}
